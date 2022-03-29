b12a0ea2e1b33bcba520c8adbf2c22e36ecc9d1b
Manuel Kienlein <manuel.kienlein@gmx.de>
2022-03-29 13:06:35 +0200

    Added unit testing

---
+++ src/test/java/de/manuelk2000/browsermicroservice/BrowserMicroserviceTest.java
@@ -0,0 +1,13 @@
+package de.manuelk2000.browsermicroservice;
+
+import org.junit.jupiter.api.Test;
+
+public class BrowserMicroserviceTest {
+
+    @Test
+    void testApplicationStart() {
+        String[] args = {};
+        BrowserMicroservice.main(args);
+    }
+
+}
